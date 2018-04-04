dbe128675beb17df839b8166b873ca24ffdc2bea
Nigel Magnay <nigel.magnay@gmail.com>
2018-04-04 14:12:08 +0100

    Use SCSS for codemirror stylesheet locally.
    Signed-off-by: Nigel Magnay <nigel.magnay@gmail.com>

--- snowglobe/server/snowglobe-server-ui/snowglobe/src/app/app.component.ts
+++ snowglobe/server/snowglobe-server-ui/snowglobe/src/app/app.component.ts
@@ -6,7 +6,7 @@ import {GlobeService} from "services/globe.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
